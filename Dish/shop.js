aa001a51fe9fdc29d331836e57f6b8a20f2a5e34
Kloeng Varin <kloengvarin11224455@gmail.com>
2026-04-01 11:47:36 +0700

    prohok_ktis and terk kroeung

--- Dish/shop.js
+++ Dish/shop.js
@@ -31,6 +31,16 @@ const links = {
         pork_belly: "https://aeononlineshopping.com/product/-/52642?store_id=1",
         mung_beans: "https://aeononlineshopping.com/product/item/16558?store_id=1",
         black_pepper: "https://aeononlineshopping.com/product/item/46010?store_id=1",
+        cucumber: "https://m.aeononlineshopping.com/product/-/15111?store_id=1",
+        cabbage: "https://m.aeononlineshopping.com/product/-/3456?store_id=1",
+        long_beans: "https://m.aeononlineshopping.com/product/-/3664?store_id=1",
+        fish_sauce: "",
+        soy_sauce: " https://m.aeononlineshopping.com/product/-/438?store_id=2",
+        palm_sugar: "",
+        oyster_sauce: "https://m.aeononlineshopping.com/product/-/39858?store_id=2",
+        rice: " https://m.aeononlineshopping.com/product/-/1915?store_id=2",
+        radish: "https://m.aeononlineshopping.com/product/-/66958?store_id=2",
+        green_onions: "https://m.aeononlineshopping.com/product/-/2487?store_id=2",
 
     },
     makro: {
@@ -62,6 +72,16 @@ const links = {
         pork_belly: "https://www.psarplus.com/en/p/uz13wwc-7606380298435?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTAlMkMlMjJxdWVyeSUyMiUzQSUyMnBvcmslMjBiZWxseSUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
         mung_beans: "https://www.psarplus.com/en/p/ZZMPG_jp-643629419587423?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTglMkMlMjJxdWVyeSUyMiUzQSUyMm11bmclMjBiZWFucyUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
         black_pepper: "https://www.psarplus.com/en/p/Vm2Oij_8-141755760260284?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTAlMkMlMjJxdWVyeSUyMiUzQSUyMmJsYWNrJTIwcGVwcGVyJTIyJTJDJTIyc291cmNlRXZlbnQlMjIlM0ElMjJzZWFyY2glMjIlMkMlMjJjYXRlZ29yeU5hbWUlMjIlM0ElMjIlMjIlMkMlMjJjb2xsZWN0aW9uSWQlMjIlM0ElMjIlMjIlN0Q",
+        cucumber: "https://www.psarplus.com/en/p/uidlmq_-6976617611459?info=JTdCJTIyYm9vc3RlZFNjb3JlJTIyJTNBMiUyQyUyMmlzU2VhcmNoUGFnZSUyMiUzQXRydWUlMkMlMjJpbmRleENsaWNrZWQlMjIlM0EwJTJDJTIycXVlcnklMjIlM0ElMjJjdWN1bWJlciUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
+        cabbage: "https://www.psarplus.com/en/p/o0iv52w-971438786910181?info=JTdCJTIyYm9vc3RlZFNjb3JlJTIyJTNBMiUyQyUyMmlzU2VhcmNoUGFnZSUyMiUzQXRydWUlMkMlMjJpbmRleENsaWNrZWQlMjIlM0ExJTJDJTIycXVlcnklMjIlM0ElMjJjdWN1bWJlciUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
+        long_beans: "https://www.psarplus.com/en/p/_bo4pcf-7422332502211?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTE1JTJDJTIycXVlcnklMjIlM0ElMjJsb25nJTIwYmVhbnMlMjIlMkMlMjJzb3VyY2VFdmVudCUyMiUzQSUyMnNlYXJjaCUyMiUyQyUyMmNhdGVnb3J5TmFtZSUyMiUzQSUyMiUyMiUyQyUyMmNvbGxlY3Rpb25JZCUyMiUzQSUyMiUyMiU3RA",
+        fish_sauce: " https://www.psarplus.com/en/p/7-DPQf3E-243313826733984?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTklMkMlMjJxdWVyeSUyMiUzQSUyMiVFMSU5RSU5MSVFMSU5RSVCOSVFMSU5RSU4MCVFMSU5RSU4RiVFMSU5RiU5MiVFMSU5RSU5QSVFMSU5RSVCOCUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
+        soy_sauce: " https://www.psarplus.com/en/p/7ykKLfZw-540085700402633?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTE1JTJDJTIycXVlcnklMjIlM0ElMjIlRTElOUUlOTElRTElOUUlQjklRTElOUUlODAlRTElOUUlOUYlRTElOUYlOEElRTElOUUlQjglRTElOUUlQTIlRTElOUYlOEElRTElOUUlQjglRTElOUUlOUMlMjIlMkMlMjJzb3VyY2VFdmVudCUyMiUzQSUyMnNlYXJjaCUyMiUyQyUyMmNhdGVnb3J5TmFtZSUyMiUzQSUyMiUyMiUyQyUyMmNvbGxlY3Rpb25JZCUyMiUzQSUyMiUyMiU3RA",
+        palm_sugar: "https://www.psarplus.com/en/p/bmt58GXA-342909899406556?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTklMkMlMjJxdWVyeSUyMiUzQSUyMnN1Z2FyJTIyJTJDJTIyc291cmNlRXZlbnQlMjIlM0ElMjJzZWFyY2glMjIlMkMlMjJjYXRlZ29yeU5hbWUlMjIlM0ElMjIlMjIlMkMlMjJjb2xsZWN0aW9uSWQlMjIlM0ElMjIlMjIlN0Q",
+        oyster_sauce: "",
+        rice: " https://www.psarplus.com/en/p/072uFc93-343268178127215?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTMlMkMlMjJxdWVyeSUyMiUzQSUyMnJpY2UlMjIlMkMlMjJzb3VyY2VFdmVudCUyMiUzQSUyMnNlYXJjaCUyMiUyQyUyMmNhdGVnb3J5TmFtZSUyMiUzQSUyMiUyMiUyQyUyMmNvbGxlY3Rpb25JZCUyMiUzQSUyMiUyMiU3RA",
+        radish: "https://www.psarplus.com/en/p/nd9Tzfv-445532213220531?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTAlMkMlMjJxdWVyeSUyMiUzQSUyMnJhZGlzaCUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
+        green_onions: "https://www.psarplus.com/en/p/0icxzsi-7275698028739?info=JTdCJTIyaXNTZWFyY2hQYWdlJTIyJTNBdHJ1ZSUyQyUyMmluZGV4Q2xpY2tlZCUyMiUzQTAlMkMlMjJxdWVyeSUyMiUzQSUyMiVFMSU5RSU5RiVFMSU5RiU5MiVFMSU5RSU5QiVFMSU5RSVCOSVFMSU5RSU4MCVFMSU5RSU4MSVFMSU5RiU5MiVFMSU5RSU5MSVFMSU5RSVCOSVFMSU5RSU5OCUyMiUyQyUyMnNvdXJjZUV2ZW50JTIyJTNBJTIyc2VhcmNoJTIyJTJDJTIyY2F0ZWdvcnlOYW1lJTIyJTNBJTIyJTIyJTJDJTIyY29sbGVjdGlvbklkJTIyJTNBJTIyJTIyJTdE",
     },
 
     deli: {
@@ -93,7 +113,16 @@ const links = {
         pork_belly: "https://delishop.asia/product/pork-belly-ruminant",
         mung_beans: "",
         black_pepper: "https://delishop.asia/product/ground-black-pepper-500g-sela-pepper",
-        
+        cucumber: "https://delishop.asia/product/cucumber",
+        cabbage: "https://delishop.asia/product/mondolkiri-green-cabbage",
+        long_beans: "https://delishop.asia/product/long-bean-300g-delishop-fresh",
+        fish_sauce:" https://delishop.asia/product/premium-fish-sauce-leang-leng-700ml",
+        soy_sauce: "https://delishop.asia/product/sweet-sauce-tiger-brand",
+        palm_sugar: "https://delishop.asia/product/palm-sugar-sovannak-500g0",
+        oyster_sauce: "https://delishop.asia/product/oyster-sauce-beksul",
+        rice: "https://delishop.asia/product/organic-semi-milled-long-grain-rice-1kg-1kg-ibis-rice",
+        radish:" https://delishop.asia/product/white-radish",
+        green_onions: "https://delishop.asia/product/spring-onion-100g-khmer-organic"
     }
 };
 
