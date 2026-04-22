2ba8fc43c28d687a392da39075d8ebd51ffd0687
Jj-124-ux <tkheang10@gmail.com>
2026-04-23 00:34:27 +0700

    more icon

--- main/nav.js
+++ main/nav.js
@@ -1,11 +1,24 @@
+// function toggleMenu() {
+//     const nav = document.getElementById('navMenu');
+//     nav.classList.toggle('active');
+// }
+
+// // Close menu when clicking a link
+// document.querySelectorAll('.nav-bar').forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.getElementById('navMenu').classList.remove('active');
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.nav-bar').forEach(link => {
+        link.addEventListener('click', () => {
+            document.getElementById('navMenu').classList.remove('active');
+        });
+    });
+});
+
 function toggleMenu() {
     const nav = document.getElementById('navMenu');
     nav.classList.toggle('active');
 }
-
-// Close menu when clicking a link
-document.querySelectorAll('.nav-bar').forEach(link => {
-    link.addEventListener('click', () => {
-        document.getElementById('navMenu').classList.remove('active');
-    });
-});
