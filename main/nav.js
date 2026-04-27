b55302c4ccf9de21144627705fc9a475ed7af0c7
punhakleapoeun <oeunpunhakleap@gmail.com>
2026-04-27 13:17:33 +0700

    hi

--- main/nav.js
+++ main/nav.js
@@ -1,14 +1,3 @@
-// function toggleMenu() {
-//     const nav = document.getElementById('navMenu');
-//     nav.classList.toggle('active');
-// }
-
-// // Close menu when clicking a link
-// document.querySelectorAll('.nav-bar').forEach(link => {
-//     link.addEventListener('click', () => {
-//         document.getElementById('navMenu').classList.remove('active');
-//     });
-// });
 
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.nav-bar').forEach(link => {
